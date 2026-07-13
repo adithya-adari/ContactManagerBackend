0697ed4ecdf0c0901f403c5f46fc95fd11075315
adithya-adari <adariadithya816@gmail.com>
2026-07-13 14:21:26 +0530

    Update MongoDB connection string to use Atlas

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ContactManager");
+        await mongoose.connect("mongodb+srv://adariadithya816_db_user:adithya@9059@cluster0.qfimjmp.mongodb.net/aditya");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
